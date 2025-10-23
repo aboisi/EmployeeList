5f7d8b4921795eb48227fbc8aa893c4003b7419f
aboisi <149761398+aboisi@users.noreply.github.com>
2025-10-23 21:04:39 +0900

    ユーザー登録機能実装

--- src/main/java/com/example/demo/domain/user/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/domain/user/service/impl/UserServiceImpl.java
@@ -35,6 +35,12 @@ public class UserServiceImpl implements UserService {
 	/** ユーザー登録 */
 	@Override
 	public void signup(MUser user) {
+		//先に社員IDを採番
+		String newId = generateUserId();
+		user.setUserId(newId); //setUserIdはMUser.javaで設定
+		
+		//DBへ登録
+		mapper.insertUser(user);
 		
 	}
 }
