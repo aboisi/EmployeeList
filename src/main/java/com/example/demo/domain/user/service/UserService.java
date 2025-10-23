e5708642c3767945543ddd7fff6f185269cc034c
aboisi <149761398+aboisi@users.noreply.github.com>
2025-10-23 19:28:02 +0900

    社員IDメソッド追加

--- src/main/java/com/example/demo/domain/user/service/UserService.java
+++ src/main/java/com/example/demo/domain/user/service/UserService.java
@@ -4,6 +4,9 @@ import com.example.demo.domain.user.model.MUser;
 
 public interface UserService {
 	
+	/** 社員IDを生成 */
+	public String generateUserId();
+	
 	/** ユーザー登録 */
 	public void signup(MUser user);
 }
