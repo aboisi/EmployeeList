64300d32731105fd44553b2decef6fa86e1231a6
aboisi <149761398+aboisi@users.noreply.github.com>
2025-10-23 20:28:27 +0900

    社員ID生成機能実装

--- src/main/java/com/example/demo/domain/user/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/domain/user/service/impl/UserServiceImpl.java
@@ -1,9 +1,12 @@
 package com.example.demo.domain.user.service.impl;
 
+import java.time.LocalDate;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.domain.user.model.MUser;
+import com.example.demo.domain.user.service.UserService;
 import com.example.demo.repository.UserMapper;
 
 @Service
@@ -12,6 +15,23 @@ public class UserServiceImpl implements UserService {
 	@Autowired
 	private UserMapper mapper;
 	
+	/** 社員ID生成 */
+	@Override
+	public String generateUserId() {
+		//レコードの総数を取得
+		int count = mapper.getUserCount();//getUserCountはUserMapper.xmlと対応
+		
+		//マシン日付(年)を取得
+		LocalDate now = LocalDate.now();
+		int year = now.getYear();
+		
+		//社員ID生成
+		String userId = String.format("%d%3d",year, count + 1);
+		
+		//呼び出し元に返す
+		return userId;
+	}
+
 	/** ユーザー登録 */
 	@Override
 	public void signup(MUser user) {
