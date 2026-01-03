081a51a34073abbe44f8b0f82c7f4423bd883d20
aboisi <149761398+aboisi@users.noreply.github.com>
2026-01-03 17:15:22 +0900

    バリデーション実行用に修正

--- src/main/java/com/example/demo/controller/SignupController.java
+++ src/main/java/com/example/demo/controller/SignupController.java
@@ -6,6 +6,8 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -43,7 +45,8 @@ public class SignupController{
 	
 	/** ユーザー登録処理 */
 	@PostMapping("/signup")
-	public String postSignup(@ModelAttribute SignupForm form) {
+	public String postSignup(@ModelAttribute @Validated SignupForm form,
+			BindingResult bindingResult) {
 		
 		//formをMUserクラスに変換
 		MUser user = modelMapper.map(form, MUser.class);
