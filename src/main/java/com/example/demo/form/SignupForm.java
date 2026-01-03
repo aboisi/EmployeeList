16a5b4abff407ad133d228de50a367ff96b027bc
aboisi <149761398+aboisi@users.noreply.github.com>
2026-01-03 16:47:35 +0900

    バリデーション実装のアノテーション追加

--- src/main/java/com/example/demo/form/SignupForm.java
+++ src/main/java/com/example/demo/form/SignupForm.java
@@ -2,13 +2,27 @@ package com.example.demo.form;
 
 import java.time.LocalDate;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+//springBootバーションが3以降はjakarta
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class SignupForm {
+	
+	@NotBlank
 	private String name;
+	
+	@NotNull
+	@DateTimeFormat(pattern = "yyyy/MM/dd")
 	private LocalDate birthday;
+	
+	@NotBlank
 	private String password;
+	
+	@NotNull
 	private String gender;
 	
 }
