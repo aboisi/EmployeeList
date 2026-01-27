d694aeb690e3f2f095513777b51882d2fbd635cb
aboisi <149761398+aboisi@users.noreply.github.com>
2026-01-27 19:47:14 +0900

    社員一覧取得メソッド追加

--- src/main/java/com/example/demo/domain/user/service/UserService.java
+++ src/main/java/com/example/demo/domain/user/service/UserService.java
@@ -1,5 +1,7 @@
 package com.example.demo.domain.user.service;
 
+import java.util.List;
+
 import com.example.demo.domain.user.model.MUser;
 
 public interface UserService {
@@ -12,5 +14,8 @@ public interface UserService {
 	
 	/** ユーザー取得(社員ID指定) */
 	public MUser findByUserId(String userId);
+	
+	/** 社員一覧取得 */
+	List<MUser> getUserList();
 }
 
