0e6d976dc565e61f1053e5c2b3654bebf9b6c2da
aboisi <149761398+aboisi@users.noreply.github.com>
2026-01-27 20:04:21 +0900

    社員一覧を取得・Modelに登録を実装

--- src/main/java/com/example/demo/controller/UserListController.java
+++ src/main/java/com/example/demo/controller/UserListController.java
@@ -1,17 +1,33 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.domain.user.model.MUser;
+import com.example.demo.domain.user.service.UserService;
 
 @Controller
 @RequestMapping("/user")
 public class UserListController {
 	
-	/** ユーザー一覧画面を表示 */
+	@Autowired
+	private UserService userService;
+	
+	/** 社員一覧画面を表示 */
 	@GetMapping("/list")
-	public String getUserList() {
-		//ユーザー一覧画面を表示
+	public String getUserList(Model model) {
+		
+		List<MUser> userList = userService.getUserList();
+		
+		//Modelに登録
+		model.addAttribute("userList" , userList);
+		
+		//社員一覧画面を表示
 		return "user/list";
 	}
 }
