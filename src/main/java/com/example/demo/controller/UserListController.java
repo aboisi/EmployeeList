897b802a0f67b87565474ebe00e852bfa528cecf
aboisi <149761398+aboisi@users.noreply.github.com>
2026-03-17 19:39:59 +0900

    formをMUserクラスに変換するよう修正

--- src/main/java/com/example/demo/controller/UserListController.java
+++ src/main/java/com/example/demo/controller/UserListController.java
@@ -2,15 +2,18 @@ package com.example.demo.controller;
 
 import java.util.List;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.domain.user.model.MUser;
 import com.example.demo.domain.user.service.UserService;
+import com.example.demo.form.UserListForm;
 
 @Controller
 @RequestMapping("/user")
@@ -19,12 +22,17 @@ public class UserListController {
 	@Autowired
 	private UserService userService;
 	
+	@Autowired
+	private ModelMapper modelMapper;
+	
 	/** 社員一覧画面を表示 */
 	@GetMapping("/list")
-//	public String getUserList(Model model) {
-	public String getUserList(MUser user, Model model) {
+	public String getUserList(@ModelAttribute UserListForm form, Model model) {
+
+		//formをMUserクラスに変換
+		MUser user = modelMapper.map(form, MUser.class);
 		
-//		MUser user = new MUser();
+		//ユーザー検索
 		List<MUser> userList = userService.getUserList(user);
 		
 		//Modelに登録
