9e77aace5f724f34eb9fc1ce3a88fbd1f2d20b55
aboisi <149761398+aboisi@users.noreply.github.com>
2026-03-24 15:30:14 +0900

    社員編集クラス作成

--- src/main/java/com/example/demo/form/UserEditForm.java
+++ src/main/java/com/example/demo/form/UserEditForm.java
@@ -1,1 +1,13 @@
 package com.example.demo.form;
+
+import java.time.LocalDate;
+
+import lombok.Data;
+
+@Data
+public class UserEditForm {
+	private String name;
+	private String password;
+	private LocalDate birthday;
+	private Integer gender;
+}
