6b9bb7fa3d914b02d161312e932112ef2668aaec
aboisi <149761398+aboisi@users.noreply.github.com>
2026-04-16 20:22:13 +0900

    ユーザー更新処理追加

--- src/main/java/com/example/demo/controller/UserEditController.java
+++ src/main/java/com/example/demo/controller/UserEditController.java
@@ -1,14 +1,18 @@
 package com.example.demo.controller;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.domain.user.model.MUser;
 import com.example.demo.domain.user.service.UserService;
+import com.example.demo.form.UserEditForm;
 
 @Controller
 @RequestMapping("/user")
@@ -17,8 +21,8 @@ public class UserEditController {
 	@Autowired
 	private UserService userService;
 	
-//	@Autowired
-//	private ModelMapper modelMapper;
+	@Autowired
+	private ModelMapper modelMapper;
 	
 	/** 編集画面を表示 */
 	@GetMapping("/edit")
@@ -28,6 +32,26 @@ public class UserEditController {
 		model.addAttribute("user", user);
 		
 		return "user/edit";
+	}
+	
+	/** ユーザー更新処理 */
+	@PostMapping("/update")
+	public String postUpdate(@ModelAttribute UserEditForm form, Model model) {
+		
+		// Form → Entityに変換
+		MUser user = modelMapper.map(form, MUser.class);
+		
+		//更新処理
+		userService.updateUserOne(
+				user.getUserId(),
+				user.getName(),
+				user.getPassword(),
+				user.getBirthday(),
+				user.getGender()
+				);
+		
+		// 一覧画面へリダイレクト
+		return "redirect:/user/list";
 	}
 	
 }
